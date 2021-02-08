b8c9d6b1431ab4c40bc8a86798bf9e6b387823ab
Oskari Virtaoja <oskari@swarmia.com>
2021-02-08 17:31:11 +0200

    Add reportErrors link

--- client/index.js
+++ client/index.js
@@ -1,8 +1,18 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache, gql, ApolloLink, HttpLink } from '@apollo/client'
+
+const reportErrors = (errorCallback) => new ApolloLink((operation, forward) => {
+    const observable = forward(operation)
+    observable.subscribe({ error: errorCallback })
+
+    return observable;
+})
 
 const client = new ApolloClient({
-    uri: 'http://localhost:4000',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    link: ApolloLink.from([
+        reportErrors(console.error),
+        new HttpLink({ uri: 'http://localhost:4000'})
+    ])
 })
 
 client.query({
