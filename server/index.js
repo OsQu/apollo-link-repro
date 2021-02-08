018b24a95a03ce6ec28bbb1859cdabd6989a5384
Oskari Virtaoja <oskari@swarmia.com>
2021-02-08 16:22:40 +0200

    Add test server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+  type Query {
+    test: String
+  }
+`
+
+const resolvers = {
+    Query: {
+        test: () => "This is a test value"
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(({url}) => {
+    console.log(`Server started at ${url} `)
+})
